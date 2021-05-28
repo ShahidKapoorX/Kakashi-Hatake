e0c99bc3dbc7ce0dedb899f634fdc066bcaa6bf2
Jass Manak <82396523+JassManak1125@users.noreply.github.com>
2021-05-28 13:22:13 +0530

    Update logomaker.py

--- MashaRoBot/modules/logomaker.py
+++ MashaRoBot/modules/logomaker.py
@@ -1,6 +1,7 @@
-from YoneRobot.events import register
-from YoneRobot import OWNER_ID
-from YoneRobot import telethn as tbot
+#credit - @Simpleboy787
+from MashaRoBot.events import register
+from MashaRoBot import OWNER_ID
+from MashaRoBot import telethn as tbot
 import os 
 from PIL import Image, ImageDraw, ImageFont
 
@@ -20,13 +21,13 @@ async def lego(event):
  await event.reply('Creating your logo...wait!')
  try:
     text = event.pattern_match.group(1)
-    img = Image.open('./YoneRobot/resources/blackbg.jpg')
+    img = Image.open('./MashaRoBot/resources/blackbg.jpg')
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
     fillcolor = "gold"
     shadowcolor = "blue"
-    font = ImageFont.truetype("./YoneRobot/resources/Chopsic.otf", 330)
+    font = ImageFont.truetype("./MashaRoBot/resources/Chopsic.otf", 330)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
@@ -34,13 +35,13 @@ async def lego(event):
     x = (image_widthz-w)/2
     y= ((image_heightz-h)/2+6)
     draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
-    fname2 = "LogoByYone.png"
+    fname2 = "LogoByEmcee.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By ᴋᴀᴋᴀꜱʜɪ ʜᴀᴛᴀᴋᴇ")
+    await tbot.send_file(event.chat_id, fname2, caption="Created By ᴋᴀᴋᴀꜱʜɪ ʜᴀᴛᴀᴋᴇ")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Report @KoraSupport, {e}')
+   await event.reply(f'Error Report @Emcee_Support, {e}')
 
 
 
@@ -60,13 +61,13 @@ async def lego(event):
  await event.reply('Creating your logo...wait!')
  try:
     text = event.pattern_match.group(1)
-    img = Image.open('./YoneRobot/resources/blackbg.jpg')
+    img = Image.open('./MashaRoBot/resources/blackbg.jpg')
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
     fillcolor = "white"
     shadowcolor = "blue"
-    font = ImageFont.truetype("./YoneRobot/resources/Maghrib.ttf", 1000)
+    font = ImageFont.truetype("./MashaRoBot/resources/Maghrib.ttf", 1000)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
@@ -74,13 +75,13 @@ async def lego(event):
     x = (image_widthz-w)/2
     y= ((image_heightz-h)/2+6)
     draw.text((x, y), text, font=font, fill="white", stroke_width=0, stroke_fill="white")
-    fname2 = "LogoByYone.png"
+    fname2 = "LogoByEmcee.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By ᴋᴀᴋᴀꜱʜɪ ʜᴀᴛᴀᴋᴇ")
+    await tbot.send_file(event.chat_id, fname2, caption="Created By ᴋᴀᴋᴀꜱʜɪ ʜᴀᴛᴀᴋᴇ")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Report @KoraSupport, {e}')
+   await event.reply(f'Error Report @Emcee_Support, {e}')
 
 file_help = os.path.basename(__file__)
 file_help = file_help.replace(".py", "")
@@ -88,7 +89,7 @@ file_helpo = file_help.replace("_", " ")
 
 
 __help__ = """
- ❍ /logo text :  Create your logo with your name
-
+ ❍ /logo text :  Create your logo with your name 
+ ❍ /wlogo text :  Create your logo with your fname2
  """
-__mod_name__ = "Logo"
+__mod_name__ = "LOGO"
