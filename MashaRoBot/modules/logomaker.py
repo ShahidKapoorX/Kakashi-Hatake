21dcca2aa1c0636a25235a3ee0875ae89212a5d1
Jass Manak <82396523+JassManak1125@users.noreply.github.com>
2021-06-02 16:14:45 +0530

    Update logomaker.py

--- MashaRoBot/modules/logomaker.py
+++ MashaRoBot/modules/logomaker.py
@@ -21,27 +21,27 @@ async def lego(event):
  await event.reply('Creating your logo...wait!')
  try:
     text = event.pattern_match.group(1)
-    img = Image.open('./MashaRoBot/resources/blackbg.jpg')
+    img = Image.open('./MashaRoBot/resources/opbg.jpg')
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
     fillcolor = "gold"
     shadowcolor = "blue"
-    font = ImageFont.truetype("./MashaRoBot/resources/Chopsic.otf", 330)
+    font = ImageFont.truetype("./MashaRoBot/resources/Vamoire Wars.otf", 330)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
     draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
     x = (image_widthz-w)/2
     y= ((image_heightz-h)/2+6)
-    draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
+    draw.text((x, y), text, font=font, fill="white", stroke_width=25, stroke_fill="pink")
     fname2 = "LogoByEmcee.png"
     img.save(fname2, "png")
     await tbot.send_file(event.chat_id, fname2, caption="Created By ᴋᴀᴋᴀꜱʜɪ ʜᴀᴛᴀᴋᴇ")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Report @Emcee_Support, {e}')
+   await event.reply(f'Error Report @Kakashi_Support, {e}')
 
 
 
@@ -67,7 +67,7 @@ async def lego(event):
     pointsize = 500
     fillcolor = "white"
     shadowcolor = "blue"
-    font = ImageFont.truetype("./MashaRoBot/resources/Maghrib.ttf", 1000)
+    font = ImageFont.truetype("./MashaRoBot/resources/Chopsic.ttf", 1000)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
